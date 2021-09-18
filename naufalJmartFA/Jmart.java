c6d5de44e19edc750d9a4fc514f7bdea6a7db7a7
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-09-18 11:44:43 +0800

    CS Modul 2 - Tambahin return product di class jmart method create()

--- naufalJmartFA/Jmart.java
+++ naufalJmartFA/Jmart.java
@@ -4,10 +4,11 @@ public class Jmart {
     public static void main(String[] args){
         
     }
-    public static void create(){
+    public static Product create(){
         PriceTag priceTag = new PriceTag(100000.0d,20.0d);
         ProductCategory category = ProductCategory.BOOK;
         Product product = new Product("Harry Potter", 1, true, priceTag ,
         category);
+        return product;
     }
 }
