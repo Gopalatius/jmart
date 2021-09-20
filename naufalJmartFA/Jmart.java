7ce9f542082330e28bb5a4924791a5c3a5984bf7
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-09-20 22:08:53 +0800

    PT Modul 2 - Hapus fungsi case study di jmart

--- naufalJmartFA/Jmart.java
+++ naufalJmartFA/Jmart.java
@@ -4,13 +4,7 @@ public class Jmart {
     public static void main(String[] args){
         
     }
-    public static Product create(){
-        PriceTag priceTag = new PriceTag(100000.0d,20.0d);
-        ProductCategory category = ProductCategory.BOOK;
-        Product product = new Product("Harry Potter", 1, true, priceTag ,
-        category);
-        return product;
-    }
+    
     public static Product createProduct(){
         PriceTag priceTag = new PriceTag(100000.0d,20.0d);
         ProductCategory category = ProductCategory.BOOK;
