176eb4fcfd71f71271e1591ae22ebc877673bc3a
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-09-23 13:54:34 +0800

    PT Modul 2 - Revisi Shipment Duration

--- naufalJmartFA/ShipmentDuration.java
+++ naufalJmartFA/ShipmentDuration.java
@@ -9,47 +9,25 @@ public class ShipmentDuration
     public static final ShipmentDuration REGULER = new ShipmentDuration(1 << 3);
     public static final ShipmentDuration KARGO = new ShipmentDuration(1 << 4);
     
-    private int bit;
+    private final int bit;
     
     private ShipmentDuration(int bit){
         this.bit =  bit;
     }
     
     public ShipmentDuration(ShipmentDuration... args){
-        
+        int bitTemp = 0;
         for (ShipmentDuration S:args){
-            if (S == ShipmentDuration.INSTANT){
-                bit |= (1 << 0);
-            }else if (S == ShipmentDuration.SAME_DAY){
-                bit |= (1 << 1);
-            }else if (S == ShipmentDuration.NEXT_DAY){
-                bit |= (1 << 2);
-            }else if (S == ShipmentDuration.REGULER){
-                bit |= (1 << 3);
-            }else if (S == ShipmentDuration.KARGO){
-                bit |= (1 << 4);
-            }
+            bitTemp |= S.bit;
             
         }
+        this.bit = bitTemp;
     }
     
     public boolean isDuration(ShipmentDuration reference){
-        if (reference == ShipmentDuration.INSTANT){
-            return (((bit & (1 << 0)) >> 0)) == 1;
-        }else if (reference == ShipmentDuration.SAME_DAY){
-            return (((bit & (1 << 1)) >> 1)) == 1;
-        }else if (reference == ShipmentDuration.SAME_DAY){
-            return (((bit & (1 << 2)) >> 2)) == 1;
-        }else if (reference == ShipmentDuration.SAME_DAY){
-            return (((bit & (1 << 3)) >> 3)) == 1;
-        }else if (reference == ShipmentDuration.SAME_DAY){
-            return (((bit & (1 << 4)) >> 4)) == 1;
-        } else{
-            return false;
-        }
+        return ((this.bit & reference.bit) != 0);
     }
     
-    
-    
+
     
 }
