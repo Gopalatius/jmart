49d0417b65555ca3778387d7deb9b449a4306050
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-10-02 11:57:21 +0800

    CS Modul 4 - RegexPhone

--- naufalJmartFA/Store.java
+++ naufalJmartFA/Store.java
@@ -1,9 +1,12 @@
 package naufalJmartFA;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Store extends Recognizable implements FileParser
 {
+    public static final String REGEX_NAME = "";
+    public static final String REGEX_PHONE = "[0-9]{9,12}";
     public String name;
     public String address;
     public String phoneNumber;
@@ -20,6 +23,14 @@ public class Store extends Recognizable implements FileParser
         this.name = name;
         this.address = address;
         this.phoneNumber = phoneNumber;
+    }
+    public boolean validate(){
+        Pattern patternPhone = Pattern.compile(REGEX_PHONE);
+        Matcher matcherPhone = patternPhone.matcher(this.phoneNumber);
+        boolean matchFoundPhone = matcherPhone.find();
+        
+        
+        return matchFoundPhone;
     }
     public boolean read(String content){
         return false;
