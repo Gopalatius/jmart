1f86b90f7a79cce80cf5a924e948a2a9fb76a9d6
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-10-04 20:55:10 +0800

    PT Modul 4 - Edit account memastikan tidak ada compile error

--- naufalJmartFA/Account.java
+++ naufalJmartFA/Account.java
@@ -1,9 +1,12 @@
 package naufalJmartFA;
 
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Account extends Recognizable implements FileParser
 {
+    public static final String REGEX_EMAIL = "";
+    public static final String REGEX_PASSWORD = "";
     public String name;
     public String email;
     public String password;
@@ -20,5 +23,17 @@ public class Account extends Recognizable implements FileParser
     }
     public String toString(){
         return "name: "+name+"\nemail: "+email+"\npassword: "+password;
+    }
+    public boolean validate(){
+        Pattern patternEmail = Pattern.compile(REGEX_EMAIL);
+        Matcher matcherEmail = patternEmail.matcher(this.email);
+        boolean matchFoundEmail = matcherEmail.find();
+        
+        Pattern patternPassword = Pattern.compile(REGEX_PASSWORD);
+        Matcher matcherPassword = patternPassword.matcher(this.name);
+        boolean matchFoundPassword = matcherPassword.find();
+        
+        return matchFoundEmail && matchFoundPassword;
+        
     }
 }
