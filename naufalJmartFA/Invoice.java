dae0657631d7d796488b4e601a9cfb1d2caae5e2
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-10-04 20:48:12 +0800

    PT Modul 4 - Update invoice dengan ditambahin ArrayList<Record>

--- naufalJmartFA/Invoice.java
+++ naufalJmartFA/Invoice.java
@@ -1,7 +1,7 @@
 package naufalJmartFA;
 
 import java.util.Date;
-
+import java.util.ArrayList;
 
 public abstract class Invoice extends Recognizable implements FileParser
 {
@@ -13,13 +13,19 @@ public abstract class Invoice extends Recognizable implements FileParser
     public enum Rating{
         NONE, BAD, NEUTRAL, GOOD;
     }
+    public class Record{
+        public Date date;
+        public String message;
+        public Status status;
+    }
     
-    public Date date;
+    public final Date date;
     public int buyerId;
     public int productId;
     public int complaintId;
     public Rating rating;
     public Status status;
+    public ArrayList<Record> history;
     
     protected Invoice (int id, int buyerId, int productId){
         super(id);
