9c29577108acf7fcc3dab66073cee3a5b2e9b193
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-11-04 15:03:06 +0700

    TP Modul 6 - Berhasil, tetapi karena RUN di Intellij jadinya ketambah folder out

--- src/naufalJmartFA/Jmart.java
+++ src/naufalJmartFA/Jmart.java
@@ -1,8 +1,35 @@
 package naufalJmartFA;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
 public class Jmart {
+    class Country
+    {
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
     public static void main(String[] args){
-        
+        // sesuaikan dengan lokasi di sistem anda kepada city.json
+        String filepath = "/My Drive/PC/Kuliah/Semester 3/Pemrograman Berorientasi Objek/Praktikum/Testing/jmart/lib/city.json";
+        Gson gson = new Gson();
+        try
+        {
+            BufferedReader br = new BufferedReader (new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states: ");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
     
 }
