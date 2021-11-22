9cd91a16b3f92b3a1e2b896daab5900cdbec8d8e
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-11-22 20:50:58 +0700

    PT Modul 8 - CouponController.java sepertinya sudah

---
+++ src/main/java/com/naufalJmartFA/controller/CouponController.java
@@ -0,0 +1,48 @@
+package com.naufalJmartFA.controller;
+
+import com.naufalJmartFA.Algorithm;
+import com.naufalJmartFA.Coupon;
+import com.naufalJmartFA.JsonTable;
+
+import com.naufalJmartFA.dbjson.JsonAutowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/coupon")
+public class CouponController implements BasicGetController<Coupon> {
+    @JsonAutowired(value = Coupon.class,filepath ="/My Drive/PC/Kuliah/Semester 3/Pemrograman Berorientasi Objek/Praktikum/Testing/jmart/lib/randomProductList.json" )
+    public static JsonTable<Coupon> couponTable;
+
+    public JsonTable<Coupon> getJsonTable(){
+        return couponTable;
+    }
+
+    @GetMapping("/{id}/canApply")
+    boolean canApply(@PathVariable int id,
+                     @RequestParam double price,
+                     @RequestParam double discount){
+        Coupon coupon = Algorithm.<Coupon>find(getJsonTable(),pred -> pred.id == id);
+        return coupon.canApply(price,discount);
+    }
+    @GetMapping("/getAvailable")
+    List<Coupon> getAvailable(@RequestParam int page,
+                              @RequestParam int pageSize){
+        List<Coupon> list1 = Algorithm.<Coupon>collect(getJsonTable(), pred -> true);
+        List<Coupon> list2 = new ArrayList<Coupon>();
+        for (Coupon i : list1){
+            if(!i.isUsed()){
+                list2.add(i);
+            }
+        }
+        return Algorithm.<Coupon>paginate(list2,page,pageSize, pred -> true);
+    }
+
+    @GetMapping("/{id}/isUsed")
+    boolean isUsed(@PathVariable int id){
+        Coupon coupon = Algorithm.<Coupon>find(getJsonTable(),pred -> pred.id == id);
+        return coupon.isUsed();
+    }
+}
