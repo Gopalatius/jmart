77c15dbc98ac5c3ba5cd5bbdb20d5563adc091f1
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-16 23:28:58 +0700

    Proyek Akhir - Javadoc untuk Coupon.java

--- src/main/java/com/naufalJmartFA/Coupon.java
+++ src/main/java/com/naufalJmartFA/Coupon.java
@@ -3,8 +3,16 @@ package com.naufalJmartFA;
 
 import com.naufalJmartFA.dbjson.Serializable;
 
+/**
+ * A coupon object. It extends serializable because
+ * coupons must have a serialized id.
+ * @author Muhammad Naufal Faza
+ */
 public class Coupon extends Serializable
 {
+    /**
+     * An enum whether the coupon is a discount or a rebate.
+     */
     public enum Type
     {
     DISCOUNT, REBATE
@@ -15,7 +23,15 @@ public class Coupon extends Serializable
     public final Type type;
     public final double minimum;
     private boolean used;
-    
+
+    /**
+     * The constructor of coupon to initialized its parameters. The used is initialized as false.
+     * @param name The name of the coupon.
+     * @param code The code of the coupon.
+     * @param type The type of the coupon.
+     * @param cut The cut of the coupon.
+     * @param minimum The minimum price to apply the coupon.
+     */
     public Coupon ( String name, int code, Type type, double cut, double minimum){
         this.name = name;
         this.code = code;
@@ -24,12 +40,31 @@ public class Coupon extends Serializable
         this.minimum = minimum;
         this.used = false;
     }
+
+    /**
+     * Check whether the coupon is used.
+     * @return true if the coupon is used. else, false.
+     */
     public boolean isUsed(){
         return this.used;
     }
+
+    /**
+     * Check if the coupon can be apply.
+     * @param price The price of the product.
+     * @param discount The discount that wants to be applied to the product.
+     * @return true if the adjusted price is more than or equal to the minimum priced and is not used.
+     */
     public boolean canApply(double price, double discount){
         return (Treasury.getAdjustedPrice(price,discount) >= minimum && !used );
     }
+
+    /**
+     * To apply the coupon,
+     * @param price The price of the product.
+     * @param discount The discount of the product
+     * @return the final money to be paid after applying the coupon.
+     */
     public double apply(double price, double discount){
         this.used = true;
         if (type == Type.DISCOUNT){
