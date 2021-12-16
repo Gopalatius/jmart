1c65108f059dc55066933b8f8e1bd21b1bdda398
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-17 00:02:52 +0700

    Proyek Akhir - Javadoc untuk Predicate.java

--- src/main/java/com/naufalJmartFA/PhoneTopUp.java
+++ src/main/java/com/naufalJmartFA/PhoneTopUp.java
@@ -1,14 +1,30 @@
 package com.naufalJmartFA;
 
+/**
+ * A feature to do PhoneTopUp
+ * @author Muhammad Naufal Faza
+ */
 public class PhoneTopUp extends Invoice {
     public String phoneNumber;
     public Status status;
 
+    /**
+     * The constructor to assign to the instance variables.
+     * @param buyerId The ID of the parameter.
+     * @param productId The ID of the product.
+     * @param phoneNumber The phone number that wants to be topped up.
+     */
     public PhoneTopUp(int buyerId, int productId, String phoneNumber){
         super(buyerId,productId);
         this.phoneNumber = phoneNumber;
 
     }
+
+    /**
+     * Overrided function
+     * @param product the product which the buyer wants to know the total pay.
+     * @return the totalPay
+     */
     @Override
     public double getTotalPay(Product product){
         return product.price* (1-product.discount);
--- src/main/java/com/naufalJmartFA/Predicate.java
+++ src/main/java/com/naufalJmartFA/Predicate.java
@@ -1,5 +1,15 @@
 package com.naufalJmartFA;
 
+/**
+ * An interface to do lambda expression of predicate
+ * @param <T> generic parameter
+ * @author Muhammad Naufal Faza
+ */
 public interface Predicate <T> {
+    /**
+     * To check whether the parameter is true if applied to the predicate.
+     * @param arg the variable that wants to be checked.
+     * @return true if the variables is true according to the predicate given in the lambda expression later.
+     */
     boolean predicate (T arg);
 }
