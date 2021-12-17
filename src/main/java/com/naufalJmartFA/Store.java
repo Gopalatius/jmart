0fcdb1793e9c38069dd1ff3abc0b8b6b672dd6be
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-17 08:38:53 +0700

    Proyek Akhir - Javadoc untuk Store.java

--- src/main/java/com/naufalJmartFA/Store.java
+++ src/main/java/com/naufalJmartFA/Store.java
@@ -4,7 +4,10 @@ import com.naufalJmartFA.dbjson.Serializable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
+/**
+ * Store object to store object inside an account (every account has the ability to create a store).
+ * @author Muhammad Naufal Faza
+ */
 public class Store extends Serializable
 {
     public static final String REGEX_NAME = "^[A-Z]{4,20}";
@@ -14,6 +17,13 @@ public class Store extends Serializable
     public String phoneNumber;
     public double balance;
 
+    /**
+     *
+     * @param name The store name
+     * @param address The address of the store
+     * @param phoneNumber The phone number of the store
+     * @param balance The balance of the store
+     */
     public Store(String name, String address, String phoneNumber, double balance)
     {
         this.name = name;
@@ -22,6 +32,10 @@ public class Store extends Serializable
         this.balance = balance;
     }
 
+    /**
+     * Validate the store according to the regular expression
+     * @return true if according to regex. Else, false.
+     */
     public boolean validate(){
         Pattern patternPhone = Pattern.compile(REGEX_PHONE);
         Matcher matcherPhone = patternPhone.matcher(this.phoneNumber);
@@ -36,6 +50,11 @@ public class Store extends Serializable
     public boolean read(String content){
         return false;
     }
+
+    /**
+     * String formatter for the store
+     * @return the store formatted as string
+     */
     public String toString(){
         return "name: "+name+"\naddress: "+address+"\nphoneNumber: "+phoneNumber+
                 "\nbalance: "+balance ;
