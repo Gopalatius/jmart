22785a27149161f083f220c36131906256ccee96
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-17 15:01:09 +0700

    Proyek Akhir - Javadoc untuk Serializable.java

--- src/main/java/com/naufalJmartFA/dbjson/Serializable.java
+++ src/main/java/com/naufalJmartFA/dbjson/Serializable.java
@@ -1,13 +1,19 @@
 package com.naufalJmartFA.dbjson;
 
 import java.util.HashMap;
-import java.util.Map;
 
+/**
+ * Class Serializable to serialize ID
+ * @author Netlab
+ */
 public class Serializable implements Comparable<Serializable>
 {
     private static HashMap<Class<?>, Integer> mapCounter = new HashMap<>();
     public final int id;
 
+    /**
+     * The constructor to serialize the ID.
+     */
     protected Serializable()
     {
         Integer counter = mapCounter.get(getClass());
@@ -16,23 +22,54 @@ public class Serializable implements Comparable<Serializable>
         this.id = counter;
     }
 
+    /**
+     * Method to set the closing ID
+     * @param clazz the class of the parameter
+     * @param id the id parameter
+     * @param <T> generic
+     * @return Integer of the class and id
+     */
     public static <T extends Serializable> Integer setClosingId(Class<T> clazz, int id)
     {
         return mapCounter.put(clazz, id);
     }
+
+    /**
+     * Method to get the closing ID
+     * @param clazz the parameter class
+     * @param <T> generic
+     * @return Integer of the closing ID
+     */
     public static <T extends Serializable> Integer getClosingId(Class<T> clazz)
     {
         return mapCounter.get(clazz);
     }
+
+    /**
+     * whether the object is equal to the instance id
+     * @param other other object
+     * @return true if equals. Else, false.
+     */
     public boolean equals(Object other)
     {
         return other instanceof Serializable && ((Serializable) other).id == id;
     }
+
+    /**
+     * whether the object is equal to the instance id
+     * @param other other serialiazable
+     * @return true if equals. Else, false.
+     */
     public boolean equals(Serializable other)
     {
         return other.id == id;
     }
 
+    /**
+     * Comparing other serializable id with instance variabel id.
+     * @param other other serializable
+     * @return integer of Integer.compare(this.id, other.id)
+     */
     @Override
     public int compareTo(Serializable other)
     {
