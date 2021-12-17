c6d303db537969bed9f6bda6012da79fe10293d3
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-17 15:14:18 +0700

    Proyek Akhir - Javadoc untuk BasicGetController.java

--- src/main/java/com/naufalJmartFA/controller/BasicGetController.java
+++ src/main/java/com/naufalJmartFA/controller/BasicGetController.java
@@ -11,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * A public interface for GetController. Other controller inherits from this class.
+ * @param <T> generic
+ * @author Muhammad Naufal Faza
+ */
 @RestController
 public interface BasicGetController <T extends Serializable> {
+    /**
+     * Getting the object by ID of the account user.
+     * @param id The account user ID
+     * @return the object that wanted to be found based on the ID
+     */
     @GetMapping("/{id}")
     public default T getById(@PathVariable int id){
         T obj = (T) Algorithm.<T>find(getJsonTable(), pred -> pred.id == id);
@@ -20,8 +30,18 @@ public interface BasicGetController <T extends Serializable> {
         return obj;
     }
 
+    /**
+     * Getter to getJsonTable
+     * @return jsonTable of the inherited class
+     */
     public abstract JsonTable<T> getJsonTable();
 
+    /**
+     * Getting the page of the inherited object later.
+     * @param page the page
+     * @param pageSize the page size
+     * @return list of paginated page.
+     */
     @GetMapping("/page")
     public default List<T> getPage(@RequestParam(defaultValue = "2") int page,@RequestParam(defaultValue = "5") int pageSize){
         return Algorithm.<T>paginate(getJsonTable(), page, pageSize, pred -> true);
