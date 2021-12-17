627aeaaa9bb36b9e5a9a281db913d6982fe6d51b
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-17 09:03:26 +0700

    Proyek Akhir - Javadoc untuk Product.java

--- src/main/java/com/naufalJmartFA/Product.java
+++ src/main/java/com/naufalJmartFA/Product.java
@@ -3,6 +3,11 @@ package com.naufalJmartFA;
 
 import com.naufalJmartFA.dbjson.Serializable;
 
+/**
+ * Product class for the product object.
+ * Extends serializable because every product has an id.
+ * @author Muhammad Naufal Faza
+ */
 public class Product extends Serializable
 {
 
@@ -14,7 +19,18 @@ public class Product extends Serializable
     public double price;
     public byte shipmentPlans;
     public int weight;
-    
+
+    /**
+     * Constructor to assign the parameter to the instance variables.
+     * @param accountId The account id (should've been the product id)
+     * @param name The name of the product
+     * @param weight The weight of the product
+     * @param conditionUsed Whether the product is already used
+     * @param price Price of the product
+     * @param discount The discount of the product
+     * @param category Category of the product
+     * @param shipmentPlans Shipment plans for the product
+     */
     public Product(int accountId, String name, int weight, boolean conditionUsed,
     double price, double discount, ProductCategory category, byte shipmentPlans ){
          this.accountId = accountId;
@@ -27,6 +43,10 @@ public class Product extends Serializable
          this.shipmentPlans = shipmentPlans;
      }
 
+    /**
+     * Method to format the product to string
+     * @return the formatted product to string
+     */
      @Override
     public String toString(){
         return "Name: "+name+"\nWeight: "+weight+"\nconditionUsed: "+conditionUsed+
