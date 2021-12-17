947c7b661045eebd9189822a34398547a716fbd5
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-17 15:20:33 +0700

    Proyek Akhir - Javadoc untuk CouponController.java

--- src/main/java/com/naufalJmartFA/controller/CouponController.java
+++ src/main/java/com/naufalJmartFA/controller/CouponController.java
@@ -10,16 +10,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Coupon controller to control coupon from front end to backend.
+ * May not needed because of deadline.
+ */
 @RestController
 @RequestMapping("/coupon")
 public class CouponController implements BasicGetController<Coupon> {
     @JsonAutowired(value = Coupon.class,filepath ="/My Drive/PC/Kuliah/Semester 3/Pemrograman Berorientasi Objek/Praktikum/Testing/jmart/lib/coupon.json" )
     public static JsonTable<Coupon> couponTable;
 
+    /**
+     * getter for couponTable
+     * @return couponTable
+     */
     public JsonTable<Coupon> getJsonTable(){
         return couponTable;
     }
 
+    /**
+     * Whether the Coupon can be applied
+     * @param id ID of the coupon
+     * @param price price of the product
+     * @param discount discount of the produc t
+     * @return true if the coupon can be applied. Else, false.
+     */
     @GetMapping("/{id}/canApply")
     boolean canApply(@PathVariable int id,
                      @RequestParam double price,
@@ -27,19 +42,31 @@ public class CouponController implements BasicGetController<Coupon> {
         Coupon coupon = Algorithm.<Coupon>find(getJsonTable(),pred -> pred.id == id);
         return coupon.canApply(price,discount);
     }
+
+    /**
+     * Get available coupon list that hasn't been used.
+     * @param page the page
+     * @param pageSize the page size
+     * @return list of available coupon
+     */
     @GetMapping("/getAvailable")
     List<Coupon> getAvailable(@RequestParam int page,
                               @RequestParam int pageSize){
-        List<Coupon> list1 = Algorithm.<Coupon>collect(getJsonTable(), pred -> true);
-        List<Coupon> list2 = new ArrayList<Coupon>();
-        for (Coupon i : list1){
+
+        List<Coupon> list1 = new ArrayList<Coupon>();
+        for (Coupon i : getJsonTable()){
             if(!i.isUsed()){
-                list2.add(i);
+                list1.add(i);
             }
         }
-        return Algorithm.<Coupon>paginate(list2,page,pageSize, pred -> true);
+        return Algorithm.<Coupon>paginate(list1,page,pageSize, pred -> true);
     }
 
+    /**
+     * Get to know if the coupon is used or not
+     * @param id ID of the coupon
+     * @return true if the coupon is used. Else, false.
+     */
     @GetMapping("/{id}/isUsed")
     boolean isUsed(@PathVariable int id){
         Coupon coupon = Algorithm.<Coupon>find(getJsonTable(),pred -> pred.id == id);
