8ce39c1849f88af56b0e7e98a725a5565d712fc0
Gopalatius <muhammad.naufal08@ui.ac.id>
2021-12-18 11:33:47 +0700

    Proyek Akhir - Update ProductController.java

--- src/main/java/com/naufalJmartFA/controller/ProductController.java
+++ src/main/java/com/naufalJmartFA/controller/ProductController.java
@@ -54,8 +54,9 @@ public class ProductController implements BasicGetController<Product>{
                     @RequestParam double discount,
                     @RequestParam ProductCategory category,
                     @RequestParam byte shipmentPlans){
-
-        return new Product(accountId,name,weight,conditionUsed,price,discount,category,shipmentPlans);
+        Product product = new Product(accountId,name,weight,conditionUsed,price,discount,category,shipmentPlans);
+        productTable.add(product);
+        return product;
     }
 
     /**
@@ -74,46 +75,75 @@ public class ProductController implements BasicGetController<Product>{
     }
 
     /**
+     * Getting all the product, literally.
+     * @param page which page
+     * @param pageSize the page size
+     * @return all the product
+     */
+    @GetMapping("/getProducts")
+    List<Product> getProducts (@RequestParam String name,
+                               @RequestParam int page,
+                               @RequestParam int pageSize,
+                               @RequestParam double lowestPrice,
+                               @RequestParam double HighestPrice,
+                               @RequestParam boolean conditionUsed,
+                               @RequestParam ProductCategory category){
+        List<Product> list1 = Algorithm.<Product>collect(getJsonTable(),pred -> pred.price >= lowestPrice &&
+                pred.price <= HighestPrice && !conditionUsed && pred.category.equals(category));
+        if (!name.isBlank()){
+            list1 = Algorithm.<Product>paginate(list1,page,pageSize,pred -> pred.name.toLowerCase().contains(name.toLowerCase()));
+        }else{
+            list1 = Algorithm.<Product>paginate(list1,page,pageSize,pred -> true);
+        }
+
+        return list1;
+    }
+
+    /**
      * Gettign the product that is filtered according to the condition
      * @param page page
      * @param pageSize page size
-     * @param accountId the ID of the account
      * @param search strings to be search
      * @param minPrice minimum price
      * @param maxPrice maximum price
      * @param category category of the product
      * @return List of all filtered product
      */
-    @GetMapping("/getProductFiltered")
-    List<Product> getProductFiltered (@RequestParam int page,
+    @GetMapping("/getFiltered")
+    List<Product> getProductFiltered(@RequestParam int page,
                                      @RequestParam int pageSize,
-                                      @RequestParam int accountId,
-                                      @RequestParam String search,
-                                      @RequestParam int minPrice,
-                                      @RequestParam int maxPrice,
-                                      @RequestParam ProductCategory category){
-        ArrayList<Product> list1 = (ArrayList<Product>) Algorithm.<Product>collect(getJsonTable(), pred -> pred.accountId == accountId &&
-                pred.category == category);
-        if (search.isBlank()){
+                                     @RequestParam boolean conditionUsed,
+                                     @RequestParam String search,
+                                     @RequestParam double minPrice,
+                                     @RequestParam double maxPrice,
+                                     @RequestParam ProductCategory category){
+        try
+        {
+            List<Product> filterPrice = null;
+            if (minPrice != 0 && maxPrice != 0) {
+                filterPrice = Algorithm.<Product>collect(productTable,prod -> prod.price >= minPrice && prod.price <= maxPrice);
+            }else {
+                filterPrice = Algorithm.<Product>collect(productTable,prod -> true);
+            }
+            List<Product> filterName = null;
+            if(!search.isEmpty()) {
+                filterName = Algorithm.<Product>collect(filterPrice,prod -> prod.name.toLowerCase().contains(search.toLowerCase()));
+            }else {
+                filterName = Algorithm.<Product>collect(filterPrice, prod -> true);
+                return filterName;
+            }
+            List<Product> filterCategory = null;
+            filterCategory = Algorithm.<Product>collect(filterName,prod -> prod.category == category);
+
+            List<Product> filterCondition = null;
+            filterCondition = Algorithm.<Product>collect(filterCategory,prod -> prod.conditionUsed == conditionUsed);
+
+            List<Product> paginated = Algorithm.<Product>paginate(filterCondition, page, pageSize, prod -> true);
+            return paginated;
+        }catch(Exception e){
+            e.printStackTrace();
             return null;
         }
-        search = search.toLowerCase();
-        ArrayList<Product> list2 = new ArrayList<Product>();
-        for (Product i : list1){
-            if (i.name.toLowerCase().contains(search)){
-                list2.add(i);
-            }
-        }
-        if (minPrice == 0 && maxPrice == 0){
-            list2 = null;
-        }else if (maxPrice == 0){
-            list2 = (ArrayList<Product>) Algorithm.<Product>collect(list2, pred -> pred.price >= minPrice);
-        }else if (minPrice == 0){
-            list2 = (ArrayList<Product>) Algorithm.<Product>collect(list2, pred -> pred.price <= maxPrice);
-        }else{
-            list2 = (ArrayList<Product>) Algorithm.<Product>collect(list2, pred -> pred.price >= minPrice && pred.price <= maxPrice);
-        }
-
-        return Algorithm.<Product>paginate(list2,page,pageSize,pred -> true);
     }
 }
+
